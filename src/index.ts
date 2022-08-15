98536deebbdd861b2d5d351930f9556e74ba06a9
James Hortman <jamesdhortman@gmail.com>
2022-08-15 17:28:44 -0400

    Rewrote prepend HTML application to make it use arrays instead of 2 values and made the function use generics

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-// your code here
+  const button = document.getElementById("prepend") as HTMLButtonElement;
+  const input1 = document.getElementById("numSpaces") as HTMLInputElement;
+  const input2 = document.getElementById("stringToPrepend") as HTMLInputElement;
+
+    button!.addEventListener("click", function () {
+    console.log(prepend([input1!.value, input2!.value]));
+    });
+
+function prepend<T>(tuple: Array<T>) : string {
+  let newString:string = "";
+  for(let i = 0; i<+tuple[0];i++)
+  {
+    newString+=" ";
+  }
+  return newString+=tuple[1];
+}
